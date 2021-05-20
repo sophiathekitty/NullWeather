0d42ecfe5509a284f4695e56569095830127b2fd
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-05-20 16:50:01 -0600

    adding models
    it didn't let me create the repo in an ignored folder... but seems ok wth it being copied over... not sure if vscode will notice the second repo... i know it can handle having multiple files from multiple repos open and will see both repos

---
+++ models/WeatherLog.php
@@ -0,0 +1,110 @@
+<?php
+
+class WeatherLogs extends clsModel {
+    public $table_name = "WeatherLogs";
+    public $fields = [
+        [
+            'Field'=>"id",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"PRI",
+            'Default'=>"",
+            'Extra'=>"auto_increment"
+        ],[
+            'Field'=>"main",
+            'Type'=>"varchar(10)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"icon",
+            'Type'=>"varchar(10)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"clouds",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"0",
+            'Extra'=>""
+        ],[
+            'Field'=>"temp",
+            'Type'=>"double",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"temp_max",
+            'Type'=>"double",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"temp_min",
+            'Type'=>"double",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"feels_like",
+            'Type'=>"double",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"humidity",
+            'Type'=>"double",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"pressure",
+            'Type'=>"double",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"wind_deg",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"wind_speed",
+            'Type'=>"double",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"description",
+            'Type'=>"varchar(100)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"created",
+            'Type'=>"datetime",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ]
+    ];
+}
+if(defined('VALIDATE_TABLES')){
+    clsModel::$models[] = new WeatherLogs();
+}
+?>
